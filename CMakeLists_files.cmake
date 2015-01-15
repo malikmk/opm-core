6b6e0abc6874ca2fc25ece99a6c04a95ba8778b9
Jørgen Kvalsvik <jorgekva@stud.ntnu.no>
2015-01-15 13:39:52 +0100

    Introduces petsc namespaces and RAII sessions.
    The Opm::petsc namespace shall contain all petsc bindings, functions and
    containers. A RAII object is provided to manage the internal petsc structure's
    lifetime in an automatic manner.

--- CMakeLists_files.cmake
+++ CMakeLists_files.cmake
@@ -50,6 +50,7 @@ list (APPEND MAIN_SOURCE_FILES
 	opm/core/linalg/LinearSolverIstl.cpp
 	opm/core/linalg/LinearSolverUmfpack.cpp
 	opm/core/linalg/LinearSolverPetsc.cpp
+	opm/core/linalg/petsc.cpp
 	opm/core/linalg/call_umfpack.c
 	opm/core/linalg/sparse_sys.c
 	opm/core/pressure/CompressibleTpfa.cpp
@@ -291,6 +292,7 @@ list (APPEND PUBLIC_HEADER_FILES
 	opm/core/linalg/LinearSolverIstl.hpp
 	opm/core/linalg/LinearSolverUmfpack.hpp
 	opm/core/linalg/LinearSolverPetsc.hpp
+	opm/core/linalg/petsc.hpp
 	opm/core/linalg/blas_lapack.h
 	opm/core/linalg/call_umfpack.h
 	opm/core/linalg/sparse_sys.h
