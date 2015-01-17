0d0f530020c3add628900f410099fd55ade0ee02
Jørgen Kvalsvik <jorgekva@stud.ntnu.no>
2015-01-17 10:56:52 +0100

    Adds doxygen to petscvector.hpp

--- opm/core/linalg/petscvector.hpp
+++ opm/core/linalg/petscvector.hpp
@@ -45,39 +45,76 @@
 namespace Opm {
 namespace petsc {
 
+    /// Class implementing C++-support for PETSc's Vec object. Provides no
+    /// extra indirection and can be used as a handle for vanilla PETSc
+    /// functions, should the need be.
+
     class vector {
         public:
             typedef PetscScalar scalar;
             typedef PetscInt size_type;
 
-            /* Just take ownership over a given Vec. */
+            /// Takes ownership of a Vec produced by some other means. The
+            /// destruction and lifetime is now managed by vector
             explicit vector( Vec );
+            /// Copy constructor
             vector( const vector& );
 
+            /// Constructor. Does not populate the vector with values.
+            /// \param[in] size     Number of elements
             explicit vector( size_type );
+            /// Constructor. Populate [0..n-1] with scalar. This is equivalent
+            /// vector v( size ); v.assign( scalar );
+            /// \param[in]  size    Number of elements
+            /// \param[in]  scalar  Value to set
             explicit vector( size_type, scalar );
 
+            /// Construct from std::vector.
+            /// \param[in] vector   std::vector to copy from
             vector( const std::vector< scalar >& );
+            /// Construct from std::vector at the indices provided by
+            /// indexset. Negative indices are ignored.
+            /// \param[in] vector   std::vector to copy from
+            /// \param[in] indexset indices to assign values from the vector.
             vector( const std::vector< scalar >&,
                     const std::vector< size_type >& indexset );
 
+            /// Construct from array. Provided to support legacy APIs and
+            /// C or C-style libraries. Works like the std::vector-constructor,
+            /// but with raw arrays.
+            /// \param[in] vector   array to copy from
+            /// \param[in] size     size of the array
             vector( const scalar*, size_type );
+            /// Construct from array at the indices provided by indexset.
+            /// Provided to support legacy APIs and C or C-style libraries.
+            /// Works like the std::vector-constructor, but with raw arrays.
+            /// \param[in] vector   array to copy from
+            /// \param[in] indexset indices to assign values from the vector.
+            /// \param[in] size     size of the array
             vector( const scalar*, const size_type* indexset, size_type );
 
-            /* implicit conversion to Vec.
-             *
-             * We want this so the class can be dropped into petsc functions.
-             */
+            /// Implicit conversion to Vec for compability with PETSc functions
             operator Vec() const;
 
+            /// Get vector size.
+            /// \param[out] size    Number of elements in the vector
             size_type size() const;
 
-            /* assign a value to -all- cells */
+            /// Assign a value to all elements in the vector.
+            /// \param[in] scalar  The value all elements will have
             void assign( scalar );
 
+            /// Add a value to all elements in the vector
+            /// \param[in] scalar   Value to add to all elements
             vector& operator+=( scalar );
+            /// Subtract a value from all elements in the vector
+            /// \param[in] scalar   Value to subtract from all elements
             vector& operator-=( scalar );
+            /// Scalar multiplication
+            /// \param[in] scalar   Value to scale with
             vector& operator*=( scalar );
+            /// Inverse scalar multiplication (division)
+            /// \param[in] scalar   Value to scale with
             vector& operator/=( scalar );
 
             /* these currently do not have to be friends (since vector is
@@ -91,7 +128,11 @@ namespace petsc {
 
             friend scalar operator*( const vector&, const vector& );
 
+            /// Equality check
+            /// \param[out] eq  Returns true if equal, false if not
             bool operator==( const vector& ) const;
+            /// Unequality check
+            /// \param[out] eq  Returns false if equal, true if not
             bool operator!=( const vector& ) const;
 
         private:
@@ -107,10 +148,14 @@ namespace petsc {
             inline Vec ptr() const;
     };
 
+    /// Calculate the dot product of two vectors
     vector::scalar dot( const vector&, const vector& );
 
+    /// Calculate the sum of all values in the vector
     vector::scalar sum( const vector& );
+    /// Find the biggest element in the vector
     vector::scalar max( const vector& );
+    /// Find the smallest element in the vector
     vector::scalar min( const vector& );
 
 }
