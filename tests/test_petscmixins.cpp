02991c1fb13aee403da161fb6551ccd91339074e
Jørgen Kvalsvik <jorgekva@stud.ntnu.no>
2015-02-12 17:14:16 +0100

    Adds newtype and explicit_bool mixins.
    This patch provides more mixins that will prove useful. The newtype (+
    mknewtype macro) provides a quick and easy way to add more semantic
    information and type safety to primary types. The explicit bool mixin provides
    functionality from C++11 that GCC4.4 does not support - explicit bool
    conversion operator.

--- tests/test_petscmixins.cpp
+++ tests/test_petscmixins.cpp
@@ -6,7 +6,7 @@
 #if HAVE_DYNAMIC_BOOST_TEST
 #define BOOST_TEST_DYN_LINK
 #endif
-#define BOOST_TEST_MODULE MatrixTest
+#define BOOST_TEST_MODULE PetscMixinTest
 #define BOOST_TEST_MAIN
 
 #include <boost/test/unit_test.hpp>
@@ -20,14 +20,14 @@
  */
 
 namespace Opm {
-namespace petsc {
+namespace Petsc {
 template<> struct deleter< int > : std::default_delete< int > {};
 }
 }
 
 
-struct unique : public Opm::petsc::uptr< int* > {
-    typedef Opm::petsc::uptr< int* > base;
+struct unique : public Opm::Petsc::uptr< int* > {
+    typedef Opm::Petsc::uptr< int* > base;
     using base::operator=;
 
     unique( int* x ) : base( x ) {}
@@ -78,4 +78,83 @@ BOOST_AUTO_TEST_CASE( assignment ) {
      */
     unique captured = capture( cap );
     BOOST_CHECK( captured.get() == cap );
+}
+
+class ebc_test : public Opm::Petsc::explicit_bool_conversion< ebc_test > {};
+bool explicit_boolean_test( const ebc_test& ) {
+    return true;
+}
+
+BOOST_AUTO_TEST_CASE( explicit_bool_conversion_operator ) {
+    /*
+     * Since we do not yet have access to the C++11 explicit conversion
+     * operator keyword, we emulate it through the so-called safe bool idiom.
+     *
+     *  See: http://en.wikibooks.org/wiki/More_C++_Idioms/Safe_bool
+     *
+     *  This code tests the mixin and makes sure it behaves as expected.
+     *  Relies on CRTP, so every bool conversion class must implement the
+     *  following function overload: explicit_boolean_test( const class& )
+     *
+     *  Adding an automatic test for the case that is NOT supposed to compile
+     *  is unfortunately rather hard, so it is currently not covered by this
+     *  automatic test case.
+     */
+
+    ebc_test ebc;
+    BOOST_CHECK( ebc );
+    BOOST_CHECK( ebc == true );
+    BOOST_CHECK( ebc != false );
+
+    /*
+     * If this is uncommented, the test should not compile.
+     *
+     * ebc_test ebc1;
+     * int x = 0;
+     * BOOST_CHECK( ebc == x );
+     * BOOST_CHECK( ebc1 == ebc );
+     */
+}
+
+enum class returned_from { newtype, rawtype };
+
+struct age : public Opm::Petsc::newtype< int > {
+    /*
+     * Pre-C++11 support (using keyword works for GCC >= 4.8)
+     */
+    template< typename T >
+    age( T&& t ) : Opm::Petsc::newtype< int >(
+            std::forward< T >( t ) ) {}
+    /*
+     * C++11-version:
+     * using Opm::petsc::newtype< int >::newtype;
+     */
+};
+
+// Test the mknewtype macro too
+mknewtype( height, int );
+
+returned_from test_newtype( age x ) {
+    return returned_from::newtype;
+}
+
+returned_from test_newtype( height x ) {
+    return returned_from::newtype;
+}
+
+returned_from test_newtype( int x ) {
+    return returned_from::rawtype;
+}
+
+BOOST_AUTO_TEST_CASE( newtypes ) {
+    int integer = 0;
+    age x = 1;
+    age y( integer );
+    height z = 2;
+
+    BOOST_CHECK( test_newtype( integer ) == returned_from::rawtype );
+    BOOST_CHECK( test_newtype( x ) == returned_from::newtype );
+    BOOST_CHECK( test_newtype( y ) == returned_from::newtype );
+    BOOST_CHECK( test_newtype( z ) == returned_from::newtype );
+    BOOST_CHECK( test_newtype( height( x ) ) == returned_from::newtype );
 }
